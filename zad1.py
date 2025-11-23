9846f965a14a2d3b4f4c99e94d244e4bdcd68fa3
Sebastian Matuszczyk <sebastian.matuszczyk@iteo.com>
2025-11-23 16:01:11 +0100

    86c6nz67r is prime

--- zad1.py
+++ zad1.py
@@ -63,3 +63,15 @@ def word_frequencies(text: str) -> dict:
     return word_frequencies
 
 
+def is_prime(n: int) -> bool:
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    sqrt_n = int(math.sqrt(n)) + 1
+    for i in range(3, sqrt_n, 2):
+        if n % i == 0:
+            return False
+    return True
