c1ddfd597c40c13124183bc9af7ee967a7b4df90
Seba0855 <matuszczyksebastian9@gmail.com>
2026-01-17 01:10:29 +0100

    Added requirements

---
+++ main.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from ultralytics import YOLO
+from utils.plate_detector import PlateDetector
+
+model = YOLO("runs/detect/train8/weights/best.pt")
+photos = list(Path("photos").glob("*.jpg"))
+labels = list(Path("photos/labels").glob("*.txt"))
+plate_detector = PlateDetector(model = model, annotations_file_path = "photos/annotations.xml", images_dir = "photos")
+
+# Default limit is 100 images, change this value to process more or fewer images
+limit = 100
+plate_detector.run(photos, labels, limit)
